b52b93edc7baa75194e53c3d9e7895a155bd7b76
estrellaangel <69270790+estrellaangel@users.noreply.github.com>
2026-02-04 11:53:20 -0700

    Change log message for server startup

--- backend/server.js
+++ backend/server.js
@@ -274,4 +274,4 @@ app.use((req, res) => {
 // Listen (ONLY ONCE)
 // ---------------------------
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Backend running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
